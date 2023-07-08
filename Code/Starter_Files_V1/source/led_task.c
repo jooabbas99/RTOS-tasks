57016738807bba5eb7da25f2d4ad5b95f4fa1d4b
youssef abbas <60302921+jooabbas99@users.noreply.github.com>
2023-07-08 21:47:05 +0300

    TASK 2

--- Code/Starter_Files_V1/source/led_task.c
+++ Code/Starter_Files_V1/source/led_task.c
@@ -2,15 +2,21 @@
 #include "led_task.h"
 
 
-void ledTask(void *pvParameters){
-
+void ledTask(void *pvParameters)
+{
+	uint16_t delay_value = 1000;
+	pinX_t pin_num = PIN1;
+	if(pvParameters != NULL){
+		delay_value = ((led_task_config *)pvParameters)->delay;
+		pin_num = ((led_task_config *)pvParameters)->pin_num;
+	}
 
 	for( ; ; ){
 
-				GPIO_write(PORT_0, PIN1, PIN_IS_HIGH);
-				vTaskDelay(1000);
-				GPIO_write(PORT_0, PIN1, PIN_IS_LOW);
-				vTaskDelay(1000);
+				GPIO_write(PORT_0, pin_num, PIN_IS_HIGH);
+				vTaskDelay(delay_value);
+				GPIO_write(PORT_0, pin_num, PIN_IS_LOW);
+				vTaskDelay(delay_value);
 
 	}
 }
--- Code/Starter_Files_V1/source/led_task.h
+++ Code/Starter_Files_V1/source/led_task.h
@@ -6,7 +6,14 @@
 #include "GPIO.h"
 #include "task.h"
 
+/* configration struct to config the task with wanted pin to toggle and its duration*/
+typedef struct{
+    pinX_t pin_num;
+    uint16_t delay;
+}led_task_config;
 
 /*task to toggle the led every 1 sec*/
 void ledTask(void *pvParameters);
+
+
 #endif
